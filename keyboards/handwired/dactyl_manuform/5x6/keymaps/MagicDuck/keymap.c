15a95fe5121385d4ac1e0fa9d191d6e53443f250
Stephan Badragan <sbadragan@xmatters.com>
2022-04-19 19:37:21 -0700

    adding key repeat functionality

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/MagicDuck/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/MagicDuck/keymap.c
@@ -2,6 +2,7 @@
 #include "keymap_combo.h" 
 #include "oneshot.h"
 #include "swapper.h"
+#include "repeat.h"
 
 enum layers {
   BASE_LAYER,
@@ -18,6 +19,7 @@ enum keycodes {
   OS_CMD,
 
   SW_WIN,  // Switch to next window         (cmd-tab)
+  REPEAT
 };
 
 #define OSL_HYPR OSM(MOD_HYPR)
@@ -90,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      RESET,   _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, RESET,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, FIND,   OSL_HYPR, RELOAD,  _______,                            KC_HOME, KC_PGUP, KC_PGDN, BSPC_LN, DEL_LN,  _______,
+     _______, _______, FIND,   OSL_HYPR, RELOAD,  REPEAT,                            KC_HOME, KC_PGUP, KC_PGDN, BSPC_LN, DEL_LN,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, OS_SHFT, OS_ALT,  OS_CTRL, OS_CMD,  SW_WIN,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_END, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼                          ┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -154,11 +156,13 @@ oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // window swapper
     update_swapper(
         &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
         keycode, record
     );
 
+    // oneshot mods
     update_oneshot(
         &os_shft_state, KC_LSFT, OS_SHFT,
         keycode, record
@@ -176,45 +180,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         keycode, record
     );
 
+    // repeat last key
+    process_repeat_key(REPEAT, keycode, record);
+
     return true;
 }
 
-
-//
-// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//
-//   [_QWERTY] = LAYOUT_5x6(
-//      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-//      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-//      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-//      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-//                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-//                                       RAISE,KC_SPC,                        KC_ENT, LOWER,
-//                                       KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-//                                       KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
-//   ),
-//
-//   [_LOWER] = LAYOUT_5x6(
-//
-//      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-//      _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-//      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-//      _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-//                                              _______,KC_PSCR,            _______, KC_P0,
-//                                              _______,_______,            _______,_______,
-//                                              _______,_______,            _______,_______,
-//                                              _______,_______,            _______,_______
-//
-//   ),
-//
-//   [_RAISE] = LAYOUT_5x6(
-//        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-//        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-//        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-//        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-//                                                _______,_______,            KC_EQL ,_______,
-//                                                _______,_______,            _______,_______,
-//                                                _______,_______,            _______,_______,
-//                                                _______,_______,            _______,_______
-//   ),
-// };
